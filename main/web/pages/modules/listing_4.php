81b81c63c49375d86e8a9beaf11dee19f2f479e9
danilo <danilo@Z50-75>
2017-03-20 02:21:42 -0300

    correccion publicidad deportes.

--- main/web/pages/modules/listing_4.php
+++ main/web/pages/modules/listing_4.php
@@ -135,7 +135,7 @@
                     <!--Publicidad horizontal-->
                     <div class="row top5" ng-show="publicidadHorizontal.length > 0">
                         <img class="img-responsive center-block" style="max-height:120px"
-                             ng-src="{{publicidadHorizontal[0].guid}}" />
+                             ng-src="{{publicidadHorizontal.thumbnailImageUrl}}" />
                     </div>
                 </div>
 
